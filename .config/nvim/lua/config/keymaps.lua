7ee79ec2c6c282aa5c6cc5bde3a54ee471e6cd3f
João Bittencourt <j.v_dias@hotmail.com>
2023-12-04 20:37:19 -0300

    fix pint when bin not available and added keymap goto blade

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Disable arrow keys on normal mode
 vim.keymap.set("n", "<Up>", ":echoe 'Use k'<CR>", { desc = "Disabled", remap = true })
 vim.keymap.set("n", "<Down>", ":echoe 'Use j'<CR>", { desc = "Disabled", remap = true })
 vim.keymap.set("n", "<Left>", ":echoe 'Use h'<CR>", { desc = "Disabled", remap = true })
 vim.keymap.set("n", "<Right>", ":echoe 'Use l'<CR>", { desc = "Disabled", remap = true })
+
+-- Go to blade file
+vim.keymap.set("n", "<leader>cb", "<cmd>GoBlade<cr>")
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -37,12 +37,14 @@ return {
 				--   extra_args = { "-i", "2", "-ci" },
 				-- },
 				pint = {
+					condition = function(ctx)
+						return vim.fs.find({ "vendor/bin/pint" }, { path = ctx.filename, upward = true })[1]
+					end,
 					meta = {
 						url = "https://github.com/laravel/pint",
 						description = "Laravel Pint is an opinionated PHP code style fixer for minimalists. Pint is built on top of PHP-CS-Fixer and makes it simple to ensure that your code style stays clean and consistent.",
 					},
 					command = util.find_executable({
-						vim.fn.stdpath("data") .. "/mason/bin/pint",
 						"vendor/bin/pint",
 					}, "pint"),
 					args = { "$FILENAME" },
